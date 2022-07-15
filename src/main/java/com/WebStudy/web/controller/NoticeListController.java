6935d36ca636c92dfa8d0ba24742886d82c478de
StartDeveloperKim <ktw550111@naver.com>
2022-07-15 22:22:53 +0900

    [Update] - Oracle DB에 있는 내용이 브라우저에 보여지지 않았는데 이는 jstl driver와 ojdbc dirver 파일이 WEB-INF에 lib 폴더에 없었기 때문이다

--- src/main/java/com/WebStudy/web/controller/NoticeListController.java
+++ src/main/java/com/WebStudy/web/controller/NoticeListController.java
@@ -7,57 +7,24 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jdt.internal.compiler.batch.Main;
+import com.WebStudy.web.entity.Notice;
 
+@WebServlet("/notice/webstudy")
 public class NoticeListController extends HttpServlet{
-	public static void main(String[] args) {
-		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@localhost:1521/xe";
-		String user = "C##TAEWOO";
-		String password = "0000";
-		String sql = "SELECT * FROM NOTICE";
-		
-		try {
-			Class.forName(driver);
-			Connection con = DriverManager.getConnection(url, user, password);
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			
-			if(rs.next()) {
-				int id = rs.getInt("ID");
-				String title = rs.getString("TITLE");
-				String content = rs.getString("CONTENT");
-				Date regdate = rs.getDate("REGDATE");
-				String writerId = rs.getString("WRITER_ID");
-				int hit = rs.getInt("HIT");
-				String phone_num = rs.getString("PHONE_NUM");
-				String email = rs.getString("EMAIL");
-				
-				System.out.printf("%d, %s, %s",id, title, content);
-			}
-			
-			
-			
-			rs.close();
-			st.close();
-			con.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		List<Notice> list = new ArrayList<>();
+		
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521/xe";
 		String user = "C##TAEWOO";
@@ -70,7 +37,7 @@ public class NoticeListController extends HttpServlet{
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
 			
-			if(rs.next()) {
+			while(rs.next()) {
 				int id = rs.getInt("ID");
 				String title = rs.getString("TITLE");
 				String content = rs.getString("CONTENT");
@@ -80,22 +47,30 @@ public class NoticeListController extends HttpServlet{
 				String phone_num = rs.getString("PHONE_NUM");
 				String email = rs.getString("EMAIL");
 				
-				System.out.print(id);
+				Notice n = new Notice(id, title, content, regdate, writerId, hit, phone_num, email);
+				
+				
+				list.add(n);
 			}
 			
-			
+			/*
+			 * for(Notice n: list) { System.out.println(n.toString()); }
+			 */
 			
 			rs.close();
 			st.close();
 			con.close();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
+			System.out.println("클래스 오류");
 			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/WEB-INF/View/notice.jsp").forward(request, response);
 		
 	}
 }
